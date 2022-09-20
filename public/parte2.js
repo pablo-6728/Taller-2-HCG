0a8b339ea696b5ed2557a2538afa272b30c2b82e
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-20 14:19:04 -0500

    texturas de los cubos

--- public/parte2.js
+++ public/parte2.js
@@ -4,7 +4,7 @@ import Stats from './jsm/libs/stats.module.js'
 
 //crear un espacio donde hayan 2 figuras y cada una tiene distintas texturas
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(30, 20, 0)
 
 const renderer = new THREE.WebGLRenderer()
@@ -27,15 +27,31 @@ mesh.receiveShadow = true
 scene.add(mesh);
 
 //añadir los cubos
-const geometry = new THREE.BoxGeometry(5, 5, 5)
-const material = new THREE.MeshLambertMaterial({}) //añadir el color
-
-const cube01 = new THREE.Mesh(geometry, material)
-cube01.position.set(0, 2.51, -10)
-const cube02 = new THREE.Mesh(geometry, material)
-cube02.position.set(0, 2.51, 10)
 //TODO: añadir las texturas a los cubos
+const texture01 = new THREE.TextureLoader().load("img/shrek_texture.jpeg")
+const material01 = new THREE.MeshLambertMaterial({
+    map: texture01,
+    side: THREE.DoubleSide
+}) 
 
+const texture02 = new THREE.TextureLoader().load("img/meme.jpg")
+const material02 = new THREE.MeshLambertMaterial({
+    map:texture02,
+    side: THREE.DoubleSide,
+})
+texture02.wrapS = THREE.RepeatWrapping
+texture02.wrapT = THREE.RepeatWrapping
+texture02.repeat.set(4,4)
+
+const geometry = new THREE.BoxGeometry(15, 15, 15)
+const cube01 = new THREE.Mesh(geometry, material01)
+cube01.position.set(0, 12.51, -10)
+const cube02 = new THREE.Mesh(geometry, material02)
+cube02.position.set(0, 12.51, 10)
+
+
+
+//añadir el color
 
 
 scene.add(cube01)
